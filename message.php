a0684475c3f8be5439806ba610e184de9a51d085
satwiktiwari22 <satwiktiwari210196@acropolis.in>
2023-11-15 22:57:11 +0530

    Add smooth scrolling, form submission functionality, and update navigation links

---
+++ message.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = 'shashi.satwik22@gmail.com'; 
+    $subject = 'New Message from Website Contact Form';
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message \n\n Regards,\n $name";
+
+    if(!empty($name) && !empty($email) && !empty($message)){
+        if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+            echo 'Invalid Email';
+        } else {
+            if(mail($to, $subject, $body)){
+                echo 'Your message has been sent';
+            } else {
+                echo 'Sorry, an error occured. Please try again later.';
+            }
+        }        
+    } else {
+        echo 'All fields are required.';
+    }
+}
+?>
--- script.js
+++ script.js
@@ -11,3 +11,44 @@ const openTab = (e, tabName) => {
   document.getElementById(tabName).style.display = "block";
   e.currentTarget.className += " active-link";
 };
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let target = document.querySelector(this.getAttribute("href"));
+    let headerOffset = 70; // height of your header or any offset
+    let elementPosition = target.offsetTop;
+    let offsetPosition = elementPosition - headerOffset;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
+  });
+});
+
+let form = document.getElementsByTagName("form");
+let statusmsg = document.getElementsByClassName("status-message");
+
+form.onSubmit = (e) => {
+  e.preventDefault();
+  statusmsg.style.display = "block";
+  let formData = new FormData(form);
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", form.getAttribute("action"), true);
+  xhr.onload = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      let response = xhr.response;
+      if (response.status == "success") {
+        statusmsg.innerHTML = response;
+        statusmsg.style.color = "green";
+        form.reset();
+      } else {
+        statusmsg.innerHTML = response;
+        statusmsg.style.color = "red";
+      }
+    }
+  };
+  xhr.send(formData);
+};
