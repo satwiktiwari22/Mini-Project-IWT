f9a1563c7b465fd75d92ab6909418ed9fa750fc8
satwiktiwari22 <satwiktiwari210196@acropolis.in>
2023-11-21 12:34:42 +0530

    changing database credentials

--- message.php
+++ message.php
@@ -4,7 +4,7 @@
     $message = $_POST['message'];
 
     // replace with your database credentials
-    $mysqli = new mysqli('localhost', 'username', 'password', 'database');
+    $mysqli = new mysqli('localhost', 'root', 'password', 'mysql');
 
     if ($mysqli->connect_error) {
         die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
